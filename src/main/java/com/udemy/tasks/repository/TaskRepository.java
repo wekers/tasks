b8f59efffa26ee4a5ec99051501cdfe3d4dd36a5
wekers <fernando@wekers.org>
2024-10-31 21:43:58 -0300

    created repository

---
+++ src/main/java/com/udemy/tasks/repository/TaskRepository.java
@@ -0,0 +1,9 @@
+package com.udemy.tasks.repository;
+
+import com.udemy.tasks.model.Task;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository extends MongoRepository<Task, String> {
+}
